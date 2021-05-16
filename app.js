e96bbd0613305ac261984aea4900f085ab8e797e
nando080 <nando080@hotmail.com>
2021-05-15 23:10:25 -0300

    game logic #1

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 const colorButtons = document.querySelectorAll('map')
 const startButton = document.querySelector('.btn-start')
 const controlButtons = document.querySelectorAll('.control-btn')
+const speedValue = document.querySelector('.speed-value')
+const levelValue = document.querySelector('.level-value')
+const timeBar = document.querySelector('.time-bar')
+
+let isGameStarted = false
+let speedLevel = 1
+let gameLevel = 1
+
+const numberOfMoves = [8, 14, 20, 31]
+const movementSpeedLimts = [700, 600, 500]
+const maxSpeedLevel = 3
+const maxGameLevel = 4
+const minSpeedAndGameLevel = 1
+
+const CPUMoves = []
+const playerMoves = []
 
 const offColors = {
     green: document.querySelector('[data-js="green-light-off"]'),
@@ -18,35 +34,106 @@ const buttonSounds = {
     control: new Audio('./sound/control.mp3')
 }
 
-let isGameStarted = false;
+const controlValues = {
+    increaseSpeed () {
+        if (speedLevel < maxSpeedLevel) {
+            speedLevel++
+            renderValueOnToDisplay(speedValue, speedLevel)
+        }
+    },
+    decreaseSpeed () {
+        if (speedLevel > minSpeedAndGameLevel) {
+            speedLevel--
+            renderValueOnToDisplay(speedValue, speedLevel)
+        }
+    },
+    increaseLevel () {
+        if (gameLevel < maxGameLevel) {
+            gameLevel++
+            renderValueOnToDisplay(levelValue, gameLevel)
+        }
+    },
+    decreaseLevel () {
+        if (gameLevel > minSpeedAndGameLevel) {
+            gameLevel--
+            renderValueOnToDisplay(levelValue, gameLevel)
+        }
+    }
+}
+
+const increaseSpeed = () => {
+    if (speedLevel < maxSpeedLevel) {
+        speedLevel++
+    }
+}
+
+const renderValueOnToDisplay = (display, value) => {
+    display.innerText = value
+}
+
+const getMaxNumberOfMoves = () => numberOfMoves[gameLevel - 1]
+
+const getSpeedLimit = () => movementSpeedLimts[speedLevel - 1]
+
+const generateRandomColorValue = () => Math.floor(Math.random() * 4) + 1
+
+const setNewCPUMove = () => {
+    if (CPUMoves.length < getMaxNumberOfMoves()) {
+        CPUMoves.push(generateRandomColorValue())
+    }
+}
+
+
+
+const startGame = () => {
+    setNewCPUMove()
+    console.log(getMaxNumberOfMoves())
+    console.log(getSpeedLimit())
+}
+
+const initializeNewGame = () => {
+    if (!isGameStarted) {
+        buttonSounds['control'].play()
+        isGameStarted = !isGameStarted
+        startGame()
+    }
+}
+
+controlButtons.forEach(item => {
+    const buttonStringName = `${item.dataset.js}`
+    item.addEventListener('click', () => {
+        if (!isGameStarted) {
+            controlValues[buttonStringName]()
+            buttonSounds['control'].play()
+        }
+    })
+})
 
 startButton.addEventListener('mousedown', () => {
-    startButton.querySelector('.btn-start-img').src = './img/btn-start-on.svg'
+    if (!isGameStarted) {
+        startButton.querySelector('.btn-start-img').src = './img/btn-start-on.svg'
+    }
 })
 
 startButton.addEventListener('mouseup', () => {
     startButton.querySelector('.btn-start-img').src = './img/btn-start-off.svg'
 })
 
-startButton.addEventListener('click', () => {
-    buttonSounds['control'].play()
-})
+startButton.addEventListener('click', initializeNewGame)
 
 colorButtons.forEach(item => {
     item.addEventListener('click', () => {
-        const buttonColor = `${item.dataset.color}`
+       if (isGameStarted) { const buttonColor = `${item.dataset.color}`
         offColors[buttonColor].classList.add('hidde-button')
         buttonSounds[buttonColor].play()
         setTimeout(() => {
             offColors[buttonColor].classList.remove('hidde-button')
-        }, 500)
+        }, 500)}
     })
 })
 
-controlButtons.forEach(item => {
-    item.addEventListener('click', () => {
-        buttonSounds['control'].play()
-    })
-})
-
-console.log(controlButtons)
+setNewCPUMove()
+setNewCPUMove()
+setNewCPUMove()
+setNewCPUMove()
+console.log(CPUMoves)
